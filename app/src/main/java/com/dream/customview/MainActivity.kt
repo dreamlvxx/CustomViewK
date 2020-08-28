af7a2946e119900f2f8b27ce69c83e5b07fb7d6b
xingxinglv <lvxingxing@kingsoft.com>
2020-08-28 16:53:58 +0800

    path progress

--- app/src/main/java/com/dream/customview/MainActivity.kt
+++ app/src/main/java/com/dream/customview/MainActivity.kt
@@ -1,30 +1,11 @@
 package com.dream.customview
 
-import android.animation.ValueAnimator
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import com.dream.customview.verify.SlideView
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        slide.setListener(object : SlideView.DragListener {
-            override fun drag(distance: Float) {
-                Log.e("xxx", "drag: $distance")
-                puzzle.setDistance(distance)
-            }
-        })
-
-        val valueni = ValueAnimator.ofFloat(0f,100f).apply {
-            duration = 500
-            addUpdateListener {
-                Log.e("xxx", "onCreate: value = " + it.animatedValue)
-            }
-            start()
-        }
     }
 }
--- app/src/main/java/com/dream/customview/path/PathView.kt
+++ app/src/main/java/com/dream/customview/path/PathView.kt
@@ -1,22 +1,81 @@
 package com.dream.customview.path
 
+import android.animation.ValueAnimator
 import android.content.Context
-import android.graphics.Canvas
+import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
+import kotlin.properties.Delegates
 
+
+/**
+ * https://www.jianshu.com/p/766584c0aeee
+ */
 class PathView : View {
-    constructor(context: Context?) : super(context)
-    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
+
+    val paint = Paint().apply {
+        color = Color.RED
+        style = Paint.Style.STROKE
+        strokeWidth = 10f
+    }
+    val path = Path()
+    lateinit var newPath : Path
+    lateinit var pathMeasure : PathMeasure
+    var mLength by Delegates.notNull<Float>()
+
+    constructor(context: Context?) : super(context){
+        initView()
+    }
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs){
+        initView()
+    }
     constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(
         context,
         attrs,
         defStyleAttr
-    )
+    ){
+        initView()
+    }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
+        newPath.reset()
+        val start: Float
+        val stop: Float
+        if (animValue <= 1) {
+            start = 0f
+            stop = mLength * animValue
+        } else {
+            start = (animValue - 1) * mLength
+            stop = mLength
+        }
+        pathMeasure.getSegment(start, stop, newPath, true)
+        canvas?.drawPath(newPath,paint)
+    }
 
+
+    var animValue by Delegates.notNull<Float>()
+
+    fun anim(){
+        val valueAnim = ValueAnimator.ofFloat(0f,2f).apply {
+            duration = 3000
+            addUpdateListener {
+                animValue = it.animatedValue as Float
+                postInvalidate()
+            }
+            repeatCount = ValueAnimator.INFINITE
+            start()
+        }
+
+    }
+
+    fun initView(){
+        newPath = Path()
+        path.addCircle(500f,500f,100f,Path.Direction.CW)
+
+        pathMeasure = PathMeasure(path,false)
+        anim()
+        mLength = pathMeasure.length
     }
 
 }
